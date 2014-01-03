e94b13b67137d7a0a6af9ef0d5823bc0e917079b
Brian Hall <bhall7@hawaii.edu>
2014-01-02 15:52:44 -1000

    added Call class, generate_call function

--- src/call.py
+++ src/call.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
+def generate_call(fulltext):
+    all_fields = fulltext.split(':')
+    genotype = all_fields[0]
+    if '/' not in genotype:
+        return None
+    else:
+        return Call(fulltext, genotype)
 
 class Call:
 
@@ -8,4 +15,16 @@ class Call:
         self.fulltext = fulltext
         self.genotype = genotype
 
+    def homozygous(self):
+        alleles = self.genotype.split('/')
+        if '.' in alleles:
+            return False
+        else:
+            return alleles[0] == alleles[1]
+        
+    def heterozygous(self):
+        alleles = self.genotype.split('/')
+        return alleles[0] != alleles[1]
 
+    def no_call(self):
+        return '.' in self.genotype
--- test/call_tests.py
+++ test/call_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from src.call import Call
+from src.call import *
 
 class TestConsoleController(unittest.TestCase):
 
@@ -9,9 +9,38 @@ class TestConsoleController(unittest.TestCase):
         self.call1 = Call("0/0:1,0:1:3:0,3,26", "0/0")
         self.call2 = Call("1/1:0,2:2:3:45,3,0", "1/1")
         self.call3 = Call("0/1:1,2:3:25:57,0,25", "0/1")
+        self.call4 = Call("./.", "./.")
 
     def test_constructor(self):
         self.assertEquals('Call', self.call1.__class__.__name__)
+        self.assertEquals("0/0:1,0:1:3:0,3,26", self.call1.fulltext)
+        self.assertEquals("1/1", self.call2.genotype)
+
+    def test_homozygous(self):
+        self.assertTrue(self.call1.homozygous())
+        self.assertTrue(self.call2.homozygous())
+        self.assertFalse(self.call3.homozygous())
+
+    def test_homozygous_false_for_no_call(self):
+        self.assertFalse(self.call4.homozygous())
+
+    def test_heterozygous(self):
+        self.assertFalse(self.call1.heterozygous())
+        self.assertFalse(self.call2.heterozygous())
+        self.assertTrue(self.call3.heterozygous())
+
+    def test_no_call(self):
+        self.assertFalse(self.call1.no_call())
+        self.assertTrue(self.call4.no_call())
+
+    def test_generate_call_good_data(self):
+        call = generate_call("0/0:1,0:1:3:0,3,26")
+        self.assertTrue(call)
+
+    def test_generate_call_bad_data(self):
+        call = generate_call("01,:835j/DF")
+        self.assertFalse(call)
+
 
 
 
