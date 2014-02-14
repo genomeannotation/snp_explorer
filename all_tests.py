c38472805dfa3fa614da0cc4cecff017a0727032
Brian Hall <bhall7@hawaii.edu>
2014-02-13 17:00:50 -1000

    vcf class, file reader function

--- all_tests.py
+++ all_tests.py
@@ -7,6 +7,7 @@ import test.snp_tests
 import test.sample_tests
 import test.group_tests
 import test.ui_controller_tests
+import test.vcf_tests
 
 # get suites from test modules
 suite1 = test.call_tests.suite()
@@ -14,6 +15,7 @@ suite2 = test.snp_tests.suite()
 suite3 = test.sample_tests.suite()
 suite4 = test.group_tests.suite()
 suite5 = test.ui_controller_tests.suite()
+suite6 = test.vcf_tests.suite()
 
 # collect suites in a TestSuite object
 suite = unittest.TestSuite()
@@ -22,6 +24,7 @@ suite.addTest(suite2)
 suite.addTest(suite3)
 suite.addTest(suite4)
 suite.addTest(suite5)
+suite.addTest(suite6)
 
 # run suite
 unittest.TextTestRunner(verbosity=2).run(suite)
