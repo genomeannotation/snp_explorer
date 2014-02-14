0eeb270985d92864165df33eecb2fc204c24c11e
Brian Hall <bhall7@hawaii.edu>
2014-02-13 15:33:17 -1000

    placeholder ui classes, cleaned up useless files, added to gitignore

--- all_tests.py
+++ all_tests.py
@@ -6,12 +6,14 @@ import test.call_tests
 import test.snp_tests
 import test.sample_tests
 import test.group_tests
+import test.ui_controller_tests
 
 # get suites from test modules
 suite1 = test.call_tests.suite()
 suite2 = test.snp_tests.suite()
 suite3 = test.sample_tests.suite()
 suite4 = test.group_tests.suite()
+suite5 = test.ui_controller_tests.suite()
 
 # collect suites in a TestSuite object
 suite = unittest.TestSuite()
@@ -19,6 +21,7 @@ suite.addTest(suite1)
 suite.addTest(suite2)
 suite.addTest(suite3)
 suite.addTest(suite4)
+suite.addTest(suite5)
 
 # run suite
 unittest.TextTestRunner(verbosity=2).run(suite)
